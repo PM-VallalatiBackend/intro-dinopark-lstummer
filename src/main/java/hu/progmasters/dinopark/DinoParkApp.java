f387126e96c471ee35513199fc524ef6118e84dd
lstummer <leslie.stummer@gmail.com>
2021-06-11 23:55:32 +0200

    A Spring 3 rétegű package struktúra kialakítása, kiegészítve a domain/dto-val.

--- src/main/java/hu/progmasters/dinopark/DinoParkApp.java
+++ src/main/java/hu/progmasters/dinopark/DinoParkApp.java
@@ -6,24 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DinoParkApp {
 
-    /*
-     1. Absztrakciók, azaz minták (tervrajzok), azaz osztályok. Ennek a finomhangolása a példányosítás.
+/*
+Modell/Domain: azok az erősebb fogalmaink, amikkel dolgozunk. Adatbázisba vannak mentve, vagy a logikában
+jelennek meg. Főnevek az üzleti logikában.
 
-     2. Encapsulation - nagy erőssége, hogy nem csak az összetartozó adatokat foglalja egybe
-     (ezt egy relációs adatbázis is tudja),
-     hanem a metódusokat is, azaz, amit az adatokkal lehet csinálni.
-     Akkor működik jól, ha ezekhez az adatokhoz nem is adunk ktlen hozzáférést kívülről.
-
-     2.1. Loose Coupling (Encapsulation része): ez a jó, amikor laza összeköttetése van az osztályoknak, azaz nem,
-     vagy minimálisan függnek egymástól.
-
-     2.2. Tight Coupling: osztályok közti szoros összeköttetés. Nem jó, mert egy osztályon végzett módosítás
-     hullámként söpör végig több, tőle függő osztályon.
-
-     3. Öröklődés.
-
-     4. Polimorfizmus.
-    */
+Dto: a domain-hez hasonlóan adathordozásra szolgáló osztályok. Ők a kisebbek, lazábban értelmezettek.
+Arra vannak, hogy az összetartozó adatokat tudjuk egyben kezelni és csak dobálgatni jobbra-balra.
+ */
 
     public static void main(String[] args) {
         SpringApplication.run(DinoParkApp.class);
