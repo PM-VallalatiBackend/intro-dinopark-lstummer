b64094cd2ea064e14b45266dd57cc87469eb1bfb
lstummer <leslie.stummer@gmail.com>
2021-06-12 00:01:43 +0200

    Enum-ban ki kell rakni a ";"-őt (DinoType Enum).

---
+++ src/main/java/hu/progmasters/dinopark/domain/DinoType.java
@@ -0,0 +1,8 @@
+package hu.progmasters.dinopark.domain;
+
+public enum DinoType {
+
+    HERBIVORE, CARNIVORE;
+
+    private String typeLowcase;
+}
---
+++ src/main/java/hu/progmasters/dinopark/domain/Dinosaur.java
@@ -0,0 +1,9 @@
+package hu.progmasters.dinopark.domain;
+
+public class Dinosaur {
+
+    private Integer id;
+    private String name;
+    private String breed;
+
+}
