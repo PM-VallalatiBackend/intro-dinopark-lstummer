2904854ffc21c254049ffaf3dfcac5ac87445a6b
lstummer <leslie.stummer@gmail.com>
2021-06-12 00:12:02 +0200

    Dinosaur class and DinoType Enum are completed in domain package.

--- src/main/java/hu/progmasters/dinopark/domain/Dinosaur.java
+++ src/main/java/hu/progmasters/dinopark/domain/Dinosaur.java
@@ -5,5 +5,44 @@ public class Dinosaur {
     private Integer id;
     private String name;
     private String breed;
+    private DinoType dinoType;
 
+    public Dinosaur() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Dinosaur setId(Integer id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Dinosaur setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public Dinosaur setBreed(String breed) {
+        this.breed = breed;
+        return this;
+    }
+
+    public DinoType getDinoType() {
+        return dinoType;
+    }
+
+    public Dinosaur setDinoType(DinoType dinoType) {
+        this.dinoType = dinoType;
+        return this;
+    }
 }
