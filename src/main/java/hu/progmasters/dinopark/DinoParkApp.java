6ddc0c763818caf037d15b000e4343bee13fbdc5
lstummer <leslie.stummer@gmail.com>
2021-06-11 22:28:08 +0200

    Checked the difference between static and non static class members. The non static called fields or "példányváltozó" or attributes. Both implementations have a whatever variable, but they are not the same.

---
+++ src/main/java/hu/progmasters/dinopark/DinoParkApp.java
@@ -0,0 +1,12 @@
+package hu.progmasters.dinopark;
+
+public class DinoParkApp {
+
+    String whatever = "Whatever";
+
+    public static void main(String[] args) {
+
+        DinoParkApp dinoParkApp = new DinoParkApp();
+        DinoParkApp dinoParkApp2 = new DinoParkApp();
+    }
+}
